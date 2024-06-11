1f61c7064c7eeb38000e35229f8c2c95ed2e4a7b
Jenn <Jenn@DESKTOP-JB>
2024-06-10 21:33:41 -0700

    Add project files.

---
+++ BackToBasics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackToBasics
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Welcome to my attempt at brushing off the rust of my C# abilities");
+            Console.ReadKey();
+        }
+    }
+}
